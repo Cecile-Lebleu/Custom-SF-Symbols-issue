5b69db378f5be382df4764e936dabc1fe6ea7384
Cécile Lebleu <cecile@cecilelebleu.com>
2024-01-30 17:40:13 -0600

    Initial Commit

---
+++ Custom-SF-Symbols-issue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Custom-SF-Symbols-issue
+//
+//  Created by Cécile Lebleu on 30/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Custom-SF-Symbols-issue/Custom_SF_Symbols_issueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_SF_Symbols_issueApp.swift
+//  Custom-SF-Symbols-issue
+//
+//  Created by Cécile Lebleu on 30/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_SF_Symbols_issueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
